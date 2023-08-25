fedaf2d9d168c8112d42569f8c257ae831d91edc
okangenc <okigenc1234@gmail.com>
2023-08-25 21:42:17 +0100

    Background and Navbar

--- main/src/App.jsx
+++ main/src/App.jsx
@@ -6,7 +6,15 @@ import Works from "./components/Works";
 
 const Container = styled.div`
   height: 100vh;
-  background-color: rebeccapurple;
+  scroll-snap-type: y mandatory;
+  scroll-behavior: smooth;
+  overflow-y: auto;
+  scrollbar-width: none;
+  color: white;
+  background: url("./img/bg.jpeg");
+  &::-webkit-scrollbar{
+    display: none;
+  }
 `;
 
 function App() {
--- main/src/components/Hero.jsx
+++ main/src/components/Hero.jsx
@@ -3,12 +3,14 @@ import styled from 'styled-components'
 
 const Section = styled.div`
   height: 100vh;
-  background-color: rebeccapurple;
+  scroll-snap-align: center;
 `;
 
 const Hero = () => {
   return (
-    <Section>Hero</Section>
+    <Section>
+      
+    </Section>
   )
 }
 
