0edbfa6655d29b5a7b2d68cf8897a0005f64faad
okangenc <okigenc1234@gmail.com>
2023-08-25 22:58:18 +0100

    Navbar and Hero component additions

--- main/src/components/Hero.jsx
+++ main/src/components/Hero.jsx
@@ -1,15 +1,60 @@
 import React from 'react'
 import styled from 'styled-components'
+import Navbar from "./Navbar";
 
 const Section = styled.div`
   height: 100vh;
   scroll-snap-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const Container = styled.div`
+  height: 100%;
+  scroll-snap-align: center;
+  width: 1400px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Left = styled.div`
+  flex: 2;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 20px;
+`;
+
+const Right = styled.div`
+  flex: 3;
+  position: relative;
+`;
+
+const Title = styled.h1`
+  font-size: 74px;
+`; // Design. Implement. Evolve.
+
+const Subheader = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
 `;
 
 const Hero = () => {
   return (
     <Section>
-      
+      <Navbar />
+      <Container>
+        <Left>
+          <Title>Learn. Apply. Innovate.</Title>
+          <Subheader> What can I do? </Subheader>
+        </Left>
+        <Right>
+
+        </Right>
+      </Container>
     </Section>
   )
 }
